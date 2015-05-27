7613e9c9ebd0fbb936549f314b9c5a7e7ea14343
Scott Henry <scottyworks@gmail.com>
2015-05-27 00:51:13 -0700

    app init and start of templating

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+#id = '(?P<id>[0-9a-fA-F]{8}[-]?[0-9a-fA-F]{4}[-]?[0-9a-fA-F]{4}[-]?[0-9a-fA-F]{4}[-]?[0-9a-fA-F]{12})'
+
+urlpatterns = (
+    url(r'^/?$', 'website.views.home', name='home'),
+    url(r'^geothermal/?$',
+        TemplateView.as_view(template_name='geothermal.html'),
+        name='geothermal'
+    ),
+)
