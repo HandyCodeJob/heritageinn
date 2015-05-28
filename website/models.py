af33d62fc0995337192c5bcb7576af4921fd0271
Scott Henry <scottyworks@gmail.com>
2015-05-27 18:05:26 -0700

    room model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Room(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("Room Name", max_length=100)
+    image = models.ImageField("Picture", upload_to="img")
+    price = models.IntegerField("Cost per night")
+    description = models.TextField("Description", max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+    def cost(self):
+        return "$%i/night" % self.price
